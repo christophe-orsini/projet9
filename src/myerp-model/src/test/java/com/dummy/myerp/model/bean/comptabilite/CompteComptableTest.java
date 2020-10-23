89e6b5c9697a751d87eb6a3ef10cb71200a0ac4c
Christophe <christophe.orsini@gmail.com>
2020-10-23 20:40:22 +0200

    Creation classes de test

---
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/CompteComptableTest.java
@@ -0,0 +1,19 @@
+package com.dummy.myerp.model.bean.comptabilite;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CompteComptableTest
+{
+	/*
+	 * RG_Compta_1
+	 * Le solde d'un compte comptable est égal à la somme des montants au débit des lignes d'écriture diminuées de la somme des montants au crédit
+	 * Si le résultat est positif, le solde est dit "débiteur", si le résultat est négatif le solde est dit "créditeur"
+	 */
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/LigneEcritureComptableTest.java
@@ -0,0 +1,23 @@
+package com.dummy.myerp.model.bean.comptabilite;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LigneEcritureComptableTest
+{
+	/*
+	 * RG_Compta_4
+	 * Les montants des lignes d'écriture sont signés et peuvent prendre des valeurs négatives
+	 */
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+	/*
+	 * RG_Compta_7
+	 * Les montants des lignes d'écritures peuvent comporter 2 chiffres maximum après la virgule
+	 */
+	
+}
