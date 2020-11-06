8d6bad4aea8a5965db35fa3f26ec60e879ef5269
Christophe <christophe.orsini@gmail.com>
2020-11-06 14:23:48 +0100

    refactoring des tests JUnit 4 -> JUnit 5
    Modification du business/pom pour inclure mockito et assertj
    Modification du pom pour inclire jacoco

--- src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
+++ src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
@@ -2,20 +2,32 @@ package com.dummy.myerp.business.impl.manager;
 
 import java.math.BigDecimal;
 import java.util.Date;
-
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
 import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
 import com.dummy.myerp.technical.exception.FunctionalException;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+public class ComptabiliteManagerImplTest
+{
+    private ComptabiliteManagerImpl managerUnderTest;
 
-public class ComptabiliteManagerImplTest {
-
-    private ComptabiliteManagerImpl manager = new ComptabiliteManagerImpl();
-
-
+    @BeforeEach
+	void setUp() throws Exception
+	{
+    	managerUnderTest = new ComptabiliteManagerImpl();
+	}
+	
+	@AfterEach
+	void CleanUp()
+	{
+		managerUnderTest = null;
+	}
+	
     @Test
     public void checkEcritureComptableUnit() throws Exception {
         EcritureComptable vEcritureComptable;
@@ -29,18 +41,27 @@ public class ComptabiliteManagerImplTest {
         vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(2),
                                                                                  null, null,
                                                                                  new BigDecimal(123)));
-        manager.checkEcritureComptableUnit(vEcritureComptable);
+        managerUnderTest.checkEcritureComptableUnit(vEcritureComptable);
     }
 
-    @Test(expected = FunctionalException.class)
-    public void checkEcritureComptableUnitViolation() throws Exception {
+    @Test
+    public void checkEcritureComptableUnitViolation() throws Exception
+    {
+    	// arrange
         EcritureComptable vEcritureComptable;
         vEcritureComptable = new EcritureComptable();
-        manager.checkEcritureComptableUnit(vEcritureComptable);
+                
+        // assert
+        assertThatExceptionOfType(FunctionalException.class).isThrownBy(() ->
+        {
+        	managerUnderTest.checkEcritureComptableUnit(vEcritureComptable);
+        }).withMessage("L'écriture comptable ne respecte pas les règles de gestion.");
     }
 
-    @Test(expected = FunctionalException.class)
-    public void checkEcritureComptableUnitRG2() throws Exception {
+    @Test
+    public void checkEcritureComptableUnitRG2() throws Exception
+    {
+    	// arrange
         EcritureComptable vEcritureComptable;
         vEcritureComptable = new EcritureComptable();
         vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
@@ -52,11 +73,17 @@ public class ComptabiliteManagerImplTest {
         vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(2),
                                                                                  null, null,
                                                                                  new BigDecimal(1234)));
-        manager.checkEcritureComptableUnit(vEcritureComptable);
+        // assert
+        assertThatExceptionOfType(FunctionalException.class).isThrownBy(() ->
+        {
+        	managerUnderTest.checkEcritureComptableUnit(vEcritureComptable);
+        }).withMessage("L'écriture comptable n'est pas équilibrée.");
     }
 
-    @Test(expected = FunctionalException.class)
-    public void checkEcritureComptableUnitRG3() throws Exception {
+    @Test
+    public void checkEcritureComptableUnitRG3() throws Exception
+    {
+    	// arrange
         EcritureComptable vEcritureComptable;
         vEcritureComptable = new EcritureComptable();
         vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
@@ -68,7 +95,10 @@ public class ComptabiliteManagerImplTest {
         vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
                                                                                  null, new BigDecimal(123),
                                                                                  null));
-        manager.checkEcritureComptableUnit(vEcritureComptable);
+     // assert
+        assertThatExceptionOfType(FunctionalException.class).isThrownBy(() ->
+        {
+        	managerUnderTest.checkEcritureComptableUnit(vEcritureComptable);
+        }).withMessage("L'écriture comptable n'est pas équilibrée.");
     }
-
 }
