072043cfa0d713dfb5d9336c6490ae30d1336fb9
Christophe <christophe.orsini@gmail.com>
2020-12-04 13:49:16 +0100

    fin des UT pour Business

--- src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
+++ src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
@@ -10,11 +10,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.TransactionStatus;
+
+import com.dummy.myerp.business.impl.TransactionManager;
 import com.dummy.myerp.consumer.dao.contrat.ComptabiliteDao;
 import com.dummy.myerp.consumer.dao.contrat.DaoProxy;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
 import com.dummy.myerp.model.bean.comptabilite.SequenceEcritureComptable;
+import com.dummy.myerp.technical.exception.FunctionalException;
 import com.dummy.myerp.technical.exception.NotFoundException;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,14 +27,15 @@ class ComptabiliteManagerImplTest
 	@Mock private EcritureComptable ecritureComptableMock;
 	@Mock private DaoProxy daoProxyMock;
 	@Mock private ComptabiliteDao comptabiliteDaoMock;
+	@Mock private TransactionManager transactionManagerMock;
 	
 	private ComptabiliteManagerImpl managerUnderTest;
 	
 	@BeforeEach
 	void setUp() throws Exception
 	{
-    	managerUnderTest = new ComptabiliteManagerImpl();
-    	managerUnderTest.configure(null, daoProxyMock, null);
+		managerUnderTest = Mockito.spy(new ComptabiliteManagerImpl());
+    	managerUnderTest.configure(null, daoProxyMock, transactionManagerMock);
 	}
 	
 	@AfterEach
@@ -100,5 +105,63 @@ class ComptabiliteManagerImplTest
 		
 		// assert
 		assertThat(ecritureComptable.getReference()).isEqualTo(expected);
+	}
+		
+	@Test
+	void insertEcritureComptable() throws FunctionalException
+	{
+		// arrange
+		TransactionStatus transactionStatusMock = Mockito.mock(TransactionStatus.class);
+		Mockito.doNothing().when(managerUnderTest).checkEcritureComptable(Mockito.any(EcritureComptable.class));
+		Mockito.when(transactionManagerMock.beginTransactionMyERP()).thenReturn(transactionStatusMock);
+		Mockito.when(daoProxyMock.getComptabiliteDao()).thenReturn(comptabiliteDaoMock);
+		Mockito.doNothing().when(comptabiliteDaoMock).insertEcritureComptable(Mockito.any(EcritureComptable.class));
+		Mockito.doNothing().when(transactionManagerMock).commitMyERP(transactionStatusMock);
+		Mockito.doNothing().when(transactionManagerMock).rollbackMyERP(null);
+		
+		// act
+		managerUnderTest.insertEcritureComptable(ecritureComptableMock);
+		
+		// assert
+		Mockito.verify(transactionManagerMock, Mockito.atLeastOnce()).commitMyERP(transactionStatusMock);
+		Mockito.verify(transactionManagerMock, Mockito.atLeastOnce()).rollbackMyERP(null);
+	}
+	
+	@Test
+	void updateEcritureComptable() throws FunctionalException
+	{
+		// arrange
+		TransactionStatus transactionStatusMock = Mockito.mock(TransactionStatus.class);
+		Mockito.when(transactionManagerMock.beginTransactionMyERP()).thenReturn(transactionStatusMock);
+		Mockito.when(daoProxyMock.getComptabiliteDao()).thenReturn(comptabiliteDaoMock);
+		Mockito.doNothing().when(comptabiliteDaoMock).updateEcritureComptable(Mockito.any(EcritureComptable.class));
+		Mockito.doNothing().when(transactionManagerMock).commitMyERP(transactionStatusMock);
+		Mockito.doNothing().when(transactionManagerMock).rollbackMyERP(null);
+		
+		// act
+		managerUnderTest.updateEcritureComptable(ecritureComptableMock);
+		
+		// assert
+		Mockito.verify(transactionManagerMock, Mockito.atLeastOnce()).commitMyERP(transactionStatusMock);
+		Mockito.verify(transactionManagerMock, Mockito.atLeastOnce()).rollbackMyERP(null);
+	}
+	
+	@Test
+	void deleteEcritureComptable() throws FunctionalException
+	{
+		// arrange
+		TransactionStatus transactionStatusMock = Mockito.mock(TransactionStatus.class);
+		Mockito.when(transactionManagerMock.beginTransactionMyERP()).thenReturn(transactionStatusMock);
+		Mockito.when(daoProxyMock.getComptabiliteDao()).thenReturn(comptabiliteDaoMock);
+		Mockito.doNothing().when(comptabiliteDaoMock).deleteEcritureComptable(Mockito.any(Integer.class));
+		Mockito.doNothing().when(transactionManagerMock).commitMyERP(transactionStatusMock);
+		Mockito.doNothing().when(transactionManagerMock).rollbackMyERP(null);
+		
+		// act
+		managerUnderTest.deleteEcritureComptable(1);
+		
+		// assert
+		Mockito.verify(transactionManagerMock, Mockito.atLeastOnce()).commitMyERP(transactionStatusMock);
+		Mockito.verify(transactionManagerMock, Mockito.atLeastOnce()).rollbackMyERP(null);
 	}
 }
